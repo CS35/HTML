09af2542a9f6f2c9182f8bd654341d95f5699e0f
Joanna <Joanna.a.s.1996@Gmail.com>
2016-05-06 11:32:21 +0300

    validate name and email in form, still have a problem with the submit

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,11 +1,65 @@
-/*-------------------log in------------------------------*/
+function ValidateName()
+{
+  var name = document.getElementById("username").value;
+  if(name.length === 0)
+  {
+    ProducePrompt("Name is Required","usernamePrompt","red");
+    return false;
+  }
+  if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/))
+  {
+    ProducePrompt("First and last name please","usernamePrompt","red");
+    return false;
+  }
+  ProducePrompt("Welcome "+name,"usernamePrompt","green");
+  return true;
+}
+function ValidateEmail()
+{
+  var email = document.getElementById("e-mail").value;
+  if (email.length === 0)
+  {
+    ProducePrompt("Email is required","EmailPrompt","red");
+    return false;
+  }
+  if(!email.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/))
+  {
+    ProducePrompt("Email Adrees is invalid","EmailPrompt","red");
+    return false;
+  }
+  ProducePrompt("Valid email Adress","EmailPrompt","green");
+  return true;
+
+}
+function ValidateSubmit()
+{
+  if (!(ValidateName()) || !(ValidateEmail()))
+  {
+    jsShow(submitprompt);
+    ProducePrompt("Form must be valid to submit","submitprompt","red");
+    return false;
+  }
+}
+function ProducePrompt(message, promptlocation, color)
+{
+  document.getElementById(promptlocation).innerHTML = message;
+  document.getElementById(promptlocation).style.color = color;
+}
+
 function myFocus(x) {
     x.style.background = "yellow";
 }
-function myBlur(x) {
-    x.style.background = "white";
+
+function jsShow(id)
+{
+  document.getElementsById(id).style.display = "block";
 }
-function validate() {
+function jsHide(id)
+{
+  document.getElementsById(id).style.display = "none";
+}
+
+/*function validate() {
 var name = document.myform.username.value;
 var pass = document.myform.pword.value;
 
@@ -38,7 +92,7 @@ var dom = document.getElementById('log').innerHTML;
 dom.visibility = "hidden"; */
 
 /*-------------------feedback------------------------------*/
-function checkForm(){
+/*function checkForm(){
 
 var fname = document.feedform.usernam.value;
 var mail = document.feedform.e-mail.value;
@@ -64,3 +118,4 @@ else if (massage === ""){
         return false;
 }
 }
+*/
