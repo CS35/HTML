f39c800c7e0a253f86c5cbfb315d9aa451b5e806
Joanna <Joanna.a.s.1996@Gmail.com>
2016-05-06 15:26:13 +0300

    still have a prob

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -3,48 +3,49 @@ function ValidateName()
   var name = document.getElementById("username").value;
   if(name.length === 0)
   {
-    ProducePrompt("Name is Required","usernamePrompt","red");
+    Error("Name is Required","usernamePrompt");
     return false;
   }
   if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/))
   {
-    ProducePrompt("First and last name please","usernamePrompt","red");
+    Error("First and last name please","usernamePrompt");
     return false;
   }
-  ProducePrompt("Welcome "+name,"usernamePrompt","green");
-  return true;
+  else
+  {
+    Pass("Welcome "+name,"usernamePrompt","green");
+    return true;
+  }
 }
 function ValidateEmail()
 {
   var email = document.getElementById("e-mail").value;
   if (email.length === 0)
   {
-    ProducePrompt("Email is required","EmailPrompt","red");
+    Error("Email is required","EmailPrompt");
     return false;
   }
   if(!email.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/))
   {
-    ProducePrompt("Email Adrees is invalid","EmailPrompt","red");
+    Error("Email Adrees is invalid","EmailPrompt","red");
     return false;
   }
-  ProducePrompt("Valid email Adress","EmailPrompt","green");
+  else{
+  Pass("Valid email Adress","EmailPrompt","green");
   return true;
-
+}
 }
 function ValidateSubmit()
 {
   if (!(ValidateName()) || !(ValidateEmail()))
   {
-    jsShow(submitprompt);
-    ProducePrompt("Form must be valid to submit","submitprompt","red");
-    setTimeout(function(){jsHide("submitprompt");},2000);
+    Error("Form must be filled to submit","submitprompt");
     return false;
   }
-}
-function ProducePrompt(message, promptlocation, color)
-{
-  document.getElementById(promptlocation).innerHTML = message;
-  document.getElementById(promptlocation).style.color = color;
+  else {
+    Pass("submitprompt");
+    return true;
+  }
 }
 
 function myFocus(x) {
@@ -54,43 +55,41 @@ function myBlur(x)
 {
   x.style.background = "white";
 }
-function jsShow(id)
-{
-  document.getElementsById(id).style.display = "block";
-}
-function jsHide(id)
-{
-  document.getElementsById(id).style.display = "none";
-}
 
 function validate() {
 var name = document.myform.username.value;
 var pass = document.myform.pword.value;
 
-if(name === "" && pass === ""){
-  document.getElementById("error").innerHTML = "<p> Error: The user name and password is empty </p>";
-  document.getElementById("error").style.color = "red";
-  document.getElementById("error").style.display = "block";
-    return false;
+if(name === "" && pass === "")
+{
+  Error("<p> user name and password are required </p>","error");
+  return false;
 }
-else if(name === ""){
-	document.getElementById("error").innerHTML = "<p> Error: username is empty </p>";
-  document.getElementById("error").style.color = "red";
-  document.getElementById("error").style.display = "block";
-    return false;
+else if(name === "")
+{
+  Error("<p> Error: username is empty </p>","error");
+  return false;
 }
-else if(pass === ""){
-	document.getElementById("error").innerHTML = "<p> Error: password is empty </p>";
-  document.getElementById("error").style.color = "red";
-  document.getElementById("error").style.display = "block";
-    return false;
+else if(pass === "")
+{
+  Error("<p> Error: password is empty </p>","error");
+  return false;
 }
-else if(name !== "" && pass !== ""){
-   document.getElementById("error").innerHTML = "";
-   document.getElementById("error").style.color = "black";
-   document.getElementById("error").style.display = "none";
-    return true;
+else if(name !== "" && pass !== "")
+{
+   Pass("error");
+   return true;
 }
 }
-
-
+function Error(message, promptlocation)
+{
+  document.getElementById(promptlocation).innerHTML = message;
+  document.getElementById(promptlocation).style.color = "red";
+  document.getElementById(promptlocation).style.display = "block";
+}
+function Pass(promptlocation)
+{
+  document.getElementById(promptlocation).innerHTML = "";
+  document.getElementById(promptlocation).style.color = "black";
+  document.getElementById(promptlocation).style.display = "none";
+}
